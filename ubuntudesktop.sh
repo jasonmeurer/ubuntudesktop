9b349cb8c536c4a9479426aaaa170cd642ccb3e8
Jason Meurer <30842226+jasonmeurer@users.noreply.github.com>
2017-12-21 11:21:52 -0500

    Update ubuntudesktop.sh

--- ubuntudesktop.sh
+++ ubuntudesktop.sh
@@ -3,5 +3,6 @@ sudo apt-get update &&
 sudo apt-get install -y xrdp &&
 sudo apt-get install -y mate-core mate-desktop-environment mate-notification-daemon &&
 sudo sudo apt-get install -y chromium-browser firefox &&
-sudo sed -i.bak '/fi/a #xrdp multiple users configuration \n mate-session \n' /etc/xrdp/startwm.sh
-sudo echo "done"
+sudo sed -i.bak '/fi/a #xrdp multiple users configuration \n mate-session \n' /etc/xrdp/startwm.sh &&
+sudo reboot
+
