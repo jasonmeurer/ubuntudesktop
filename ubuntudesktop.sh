5383b59d71b73d3ace69220245a6790336e297cf
jasonmeurer <jasonmeurer32@gmail.com>
2017-12-20 22:06:19 -0500

    first commit

---
+++ ubuntudesktop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get update &&
+sudo apt-get install -y xrdp &&
+sudo apt-get install -y mate-core mate-desktop-environment mate-notification-daemon &&
+sudo sudo apt-get install -y chromium-browser firefox &&
+sudo sed -i.bak '/fi/a #xrdp multiple users configuration \n mate-session \n' /etc/xrdp/startwm.sh
+sudo echo "done"
