b970956938f23137597adb1089a9b28785db3898
Jason Meurer <30842226+jasonmeurer@users.noreply.github.com>
2017-12-21 20:30:31 -0500

    Update ubuntudesktop.sh

--- ubuntudesktop.sh
+++ ubuntudesktop.sh
@@ -2,5 +2,5 @@
 sudo apt-get update &&
 sudo apt-get install -y xfce4 &&
 sudo apt-get install -y xrdp chromium-browser &&
-echo xfce4-session >~/.xsession &&
+sudo echo xfce4-session >~/.xsession &&
 sudo service xrdp restart
